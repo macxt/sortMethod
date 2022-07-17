c600ba6aec2f4457d9ba68c577a792b30171dbfa
yabin.ma <yabin.ma@i-tudou.com>
2022-07-17 13:30:55 +0800

    修改测试用例起始位置及结束位置——master

--- sort_method/main.cpp
+++ sort_method/main.cpp
@@ -9,11 +9,13 @@ int main(int argc, char **argv)
 	std::vector<int> vSortArr = { 2, 7, 4, 8, 0, -1 };
 	std::vector<int> vSortArr2 = { -99, -200, 4, 1, 0, 17, -173, 15 };
 	std::vector<double> vSortArr3 = { -9.787, -20.21932, 4.32, 1.0001, 0.891, 17.27609, -173.21734, 15.28763 };
+	std::vector<float> vfloatArr = { 2.345, -0.231, 42.891, 21.7436, 23.90876, 213.89301 };
 	CSort csort;
 
 	csort.SelectSort<false>(vSortArr.begin()+1, vSortArr.end()-1);
 	std::cout << vSortArr << std::endl;
 
+	csort.SelectSort(vfloatArr.begin() + 2, vfloatArr.end() - 1);
 
 	csort.BubbleSort<false>(vSortArr3.begin()+1, vSortArr3.end()-1);
 	
